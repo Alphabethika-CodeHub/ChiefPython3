f20b513f32d721699471141d27fa25ffcb765837
Daffa6633 <Daffa6633@gmail.com>
2022-10-13 17:13:40 +0700

    Initial

---
+++ main.py
@@ -0,0 +1,20 @@
+myInt = 4
+myFloat = 5.0
+myPI = 3.14
+myString = "Hello World!"
+
+# Calculate Area of Rectangular.
+recLength = 5
+recWide = 12
+areaOfRectangular = 2 * (recLength * recWide)
+print("Area of Rectangular: " + str(areaOfRectangular))
+
+# Concate String With Number
+myString1 = "Hello "
+myString2 = "World "
+myInteger3 = 3.0
+print(myString1 + myString2 + str(myInteger3))
+
+
+
+
