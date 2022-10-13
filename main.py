6ea2c53c6eefd69b7653a8bfc26c532ffb23cfa1
Daffa6633 <Daffa6633@gmail.com>
2022-10-13 20:48:36 +0700

    Initial

--- main.py
+++ main.py
@@ -1,19 +1,45 @@
+import math
+
 myInt = 4
 myFloat = 5.0
 myPI = 3.14
 myString = "Hello World!"
 
 # Calculate Area of Rectangular.
-recLength = 5
-recWide = 12
-areaOfRectangular = 2 * (recLength * recWide)
+REC_LENGTH = 5
+REC_WIDE = 12
+areaOfRectangular = REC_LENGTH * REC_WIDE
+perimeterOfRectangular = 2 * (REC_LENGTH + REC_WIDE)
+diagonalRectangular = math.sqrt((REC_LENGTH ** 2) + (REC_WIDE ** 2))
 print("Area of Rectangular: " + str(areaOfRectangular))
+print("Perimeter of Rectangular: " + str(perimeterOfRectangular))
+print("Diagonal of Rectangular: " + str(diagonalRectangular))
 
-# Concate String With Number
+# Concatenated String With Number
 myString1 = "Hello "
 myString2 = "World "
 myInteger3 = 3.0
 print(myString1 + myString2 + str(myInteger3))
+
+MULTI_LINE_STRING = """ 
+ This is
+ Multiline
+ String by Python!
+ """
+
+def score_checker(score):
+    if(score => 90):
+        print("Grade A")
+    elif(score => 70):
+        print("Grade B")
+    elif(score => 40):
+        print("Grade C")
+    else: print("Learn is The Best Way!")
+
+
+
+
+
 
 
 
